2205f485ab584661a6e31c3a2b18a6fe278b0af7
marc25v <marc25v@users.noreply.github.com>
2020-12-27 14:41:45 -0500

    Update main.cpp

--- rpi/main.cpp
+++ rpi/main.cpp
@@ -75,7 +75,7 @@ int main()
     //
     // Set the sample rate
     //
-    if(rtlsdr_set_sample_rate(dev, 2500000) < 0)
+    if(rtlsdr_set_sample_rate(dev, 500000) < 0)
     {
         std::cout << "Failed to set sample rate" << std::endl;
         return -1;
